4267f586f7200529715e7b9c032294111fca0aa6
matalbodev <mathieualbore@gmail.com>
2023-03-23 08:45:41 +0100

    Update (navigation) chaging icons

--- components/dashboard/Navigation.tsx
+++ components/dashboard/Navigation.tsx
@@ -1,56 +1,63 @@
-import Link from "next/link"
-import { useRouter } from "next/router"
-import clsx from "clsx"
-import { CalendarIcon, FolderIcon, UserIcon } from "@heroicons/react/24/outline"
+import Link from "next/link";
+import { useRouter } from "next/router";
+import clsx from "clsx";
+import {
+  CalendarIcon,
+  FolderIcon,
+  UserIcon,
+  LightBulbIcon,
+} from "@heroicons/react/24/outline";
 
 const links = [
-	{
-		href: "/dashboard",
-		label: "Calendar",
-		icon: <CalendarIcon className="h-6 w-6 mr-2" />,
-	},
-	{
-		href: "/dashboard/meals",
-		label: "Your meals",
-		icon: <FolderIcon className="h-6 w-6 mr-2" />,
+  {
+    href: "/dashboard",
+    label: "Calendar",
+    icon: <CalendarIcon className="mr-2 h-6 w-6" />,
+  },
+  {
+    href: "/dashboard/meals",
+    label: "Your meals",
+    icon: <LightBulbIcon className="mr-2 h-6 w-6" />,
   },
   {
     href: "/dashboard/ingredients",
     label: "Ingredients",
-    icon: <FolderIcon className="h-6 w-6 mr-2" />,
+    icon: <FolderIcon className="mr-2 h-6 w-6" />,
   },
-	{
-		href: "/dashboard/profile",
-		label: "Your profile",
-		icon: <UserIcon className="h-6 w-6 mr-2" />,
-	},
-]
+  {
+    href: "/dashboard/profile",
+    label: "Your profile",
+    icon: <UserIcon className="mr-2 h-6 w-6" />,
+  },
+];
 
 const Navigation = () => {
-	const router = useRouter()
-	const { pathname } = router
+  const router = useRouter();
+  const { pathname } = router;
 
-	return (
-		<nav>
-			<ul>
-				{links.map(({ href, label, icon }, index) => (
-					<li key={index}>
-						<Link
-							className={`flex items-center px-10 border-l-4  py-6 text-md font-semibold transition-colors hover:bg-neutral-200 ${clsx(
-								href === pathname ? "bg-neutral-200 text-stone-900 border-neutral-800" : "border-transparent"
-							)}`}
-							href={href}
-						>
-							<>
-								{icon && icon}
-								{label}
-							</>
-						</Link>
-					</li>
-				))}
-			</ul>
-		</nav>
-	)
-}
+  return (
+    <nav>
+      <ul>
+        {links.map(({ href, label, icon }, index) => (
+          <li key={index}>
+            <Link
+              className={`text-md flex items-center border-l-4  px-10 py-6 font-semibold transition-colors hover:bg-neutral-200 ${clsx(
+                href === pathname
+                  ? "border-neutral-800 bg-neutral-200 text-stone-900"
+                  : "border-transparent"
+              )}`}
+              href={href}
+            >
+              <>
+                {icon && icon}
+                {label}
+              </>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
