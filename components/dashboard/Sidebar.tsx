7387fb0cea1b6017b3e62150546633c9ca35eec0
matalbodev <mathieualbore@gmail.com>
2023-03-23 08:46:29 +0100

    Update (styles)

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -1,15 +1,15 @@
-import Navigation from "./Navigation"
+import Navigation from "./Navigation";
 
 const DashBoardSidebar = () => {
-	return (
-		<div className="w-1/4 bg-neutral-100 shrink-0 pb-10">
-			<div className="bg-easeal-blue px-10 py-16">
-				<h1 className="text-white text-4xl font-semibold">Easeal</h1>
-				<p className="text-white">Your easy meal planner</p>
-			</div>
-			<Navigation />
-		</div>
-	)
-}
+  return (
+    <div className="w-1/5 shrink-0 bg-neutral-100 pb-10">
+      <div className="bg-easeal-blue px-10 py-8">
+        <h1 className="text-4xl font-semibold text-white">Easeal</h1>
+        <p className="text-white">Your easy meal planner</p>
+      </div>
+      <Navigation />
+    </div>
+  );
+};
 
-export default DashBoardSidebar
+export default DashBoardSidebar;
